0d0e8d7d6c94e2040bf856a64984d29e7b09d849
AmrutaZiradkar <jwamruta@gmail.com>
2025-05-17 17:00:09 +0530

    changed file content

--- amrutaTest.js
+++ amrutaTest.js
@@ -1,2 +1,5 @@
-const string="Hello Amruta, Good Morning !!"
+const string="Hello Amruta, Good Evening, How r u >>? !!"
 console.log(string);
+
+console.log("I love my India.......")
+console.log("I am Beautiful.........")
